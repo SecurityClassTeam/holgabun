0200c2e1b3e30904e68ed6c206c0a71181bf3434
joseunghyo <seunghyo7742@naver.com>
2022-05-09 18:06:19 +0900

    Edit Homepage, Navbar

--- holgabun_app/src/App.js
+++ holgabun_app/src/App.js
@@ -4,18 +4,22 @@ import Mypage from './routes/Mypage';
 import SignupHost from './routes/SignupHost';
 import Search_home from './routes/Search_home';
 import Account from './routes/Account'; //로그인 회원가입 페이지
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/mypage" element={<Mypage />} />
-        <Route path="/signupHost" element={<SignupHost />} />
-        <Route path="/search/home" element={<Search_home />} />
-        <Route path="/account" element={<Account />} />
-      </Routes>
-    </Router>
+    <>
+      <Navbar/>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/mypage" element={<Mypage />} />
+          <Route path="/signupHost" element={<SignupHost />} />
+          <Route path="/search/home" element={<Search_home />} />
+          <Route path="/account" element={<Account />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
