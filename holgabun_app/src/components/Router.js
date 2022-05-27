3a57f2ba5097941f6ef283f2d259ccf55a34e997
Hayeon <hayeon.official365@gmail.com>
2022-05-27 11:14:40 +0900

    import

--- holgabun_app/src/components/Router.js
+++ holgabun_app/src/components/Router.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from '../routes/Home';
+import Mypage from '../routes/Mypage';
 import Account from './Account';
 import Create from '../routes/Create';
 import HostManage from './HostManage';
-import Mypage from '../routes/Mypage';
-import SignupHost from './SignupHost';
+import Search from '../routes/Search'
+import HostPage from '../routes/HostPage';
+
+
 
 const AppRouter = ({ isLoggedIn, userObj, hostState }) => {
   // hostState가 true면
@@ -15,7 +19,7 @@ const AppRouter = ({ isLoggedIn, userObj, hostState }) => {
         <Route path="/mypage" element={<Mypage />} />
         <Route path="/account" element={<Account />} />
 
-        <Route path="/hostpage" element={<HostPage hostState={hostState}/>} />
+        <Route path="/hostpage" element={<HostPage hostState={hostState} />} />
         <Route path="/hostpage/create" element={<Create />} />
         <Route path="/hostpage/manage" element={<HostManage />} />
 
